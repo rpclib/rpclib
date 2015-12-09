3b849f4f67e1b77413bf1857e11bda72fc02f51c
sztomi <hello.sztomi@gmail.com>
2015-12-09 23:57:44 +0100

    Fixed run_clang_check.sh script

--- run_clang_check.sh
+++ run_clang_check.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-find ./src -name '*.cc' | xargs clang-check -analyze -p .
+find ./lib -name '*.cc' | xargs clang-check -analyze -p .
 find ./tests -name '*.cc' | xargs clang-check -analyze -p .
