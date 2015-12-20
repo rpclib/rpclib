f53fbd946f19fb924e722049c2297c3b4137462b
sztomi <szelei.t@gmail.com>
2015-12-20 14:13:53 +0100

    More MSVC compatibility (#27)

--- cmake/compiler_specific.cmake
+++ cmake/compiler_specific.cmake
@@ -1,11 +1,11 @@
 # Compiler-specific options
 
-if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
+if (${CMAKE_CXX_COMPILER_ID} MATCHES "Clang")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -pedantic")
-elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+elseif (${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU")
   # using GCC
-elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel")
+elseif (${CMAKE_CXX_COMPILER_ID} STREQUAL "Intel")
   # using Intel C++
-elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
-    add_definitions(-DWIN32_LEAN_AND_MEAN -D_CRT_SECURE_NO_WARNINGS)
+elseif (${CMAKE_CXX_COMPILER_ID} STREQUAL "MSVC")
+    add_definitions(-DWIN32_LEAN_AND_MEAN -D_CRT_SECURE_NO_WARNINGS -D_GNU_SOURCE)
 endif()
