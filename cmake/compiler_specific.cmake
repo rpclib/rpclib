1640c0b8f77f282083d52287a7422384c5d682e4
sztomi <szelei.t@gmail.com>
2015-12-21 22:42:30 +0100

    Install g++-5 and build coverage info with it instead of clang (#29)

--- cmake/compiler_specific.cmake
+++ cmake/compiler_specific.cmake
@@ -2,11 +2,10 @@
 
 if (${CMAKE_CXX_COMPILER_ID} MATCHES "Clang")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -pedantic")
-    if(coverage)
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -coverage -O0")
-    endif()
 elseif (${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU")
-  # using GCC
+    if(coverage)
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g --coverage -O0")
+    endif()
 elseif (${CMAKE_CXX_COMPILER_ID} STREQUAL "Intel")
   # using Intel C++
 elseif (${CMAKE_CXX_COMPILER_ID} STREQUAL "MSVC")
