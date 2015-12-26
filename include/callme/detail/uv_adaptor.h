95ebf81f859462eb14962b1c3302310d965bbff0
sztomi <szelei.t@gmail.com>
2015-12-26 11:53:19 +0100

    Added uv_adaptor to avoid repetition

---
+++ include/callme/detail/uv_adaptor.h
@@ -0,0 +1,42 @@
+#pragma once
+
+namespace callme {
+namespace detail {
+
+//! \brief Adapts static callback to object member functions.
+//! The object must may use the fw_on* functions as libuv callbacks,
+//! as long as the following is true:
+//! - The class has a function with the expected signature, without the fw_
+//! prefix.
+//! - The data members in the libuv callbacks are pointers to the object.
+template<typename Adapted>
+class uv_adaptor
+{
+protected:
+    //! \defgroup Static callbacks that forward calls to members using
+    //! the passed data pointer.
+    //! @{
+    //! \brief Forwards the on_new_connection callback to a member function.
+    static void fw_on_new_connection(uv_stream_t *stream, int status) {
+        auto obj = reinterpret_cast<Adapted *>(stream->data);
+        obj->on_new_connection(stream, status);
+    }
+
+    //! \brief Forwards the on_read callback to a member function.
+    static void fw_on_read(uv_stream_t *stream, ssize_t nread,
+                           const uv_buf_t *buf) {
+        auto obj = reinterpret_cast<Adapted *>(stream->data);
+        obj->on_read(stream, nread, buf);
+    }
+
+    //! \brief Forwards the alloc_buffer callback to a member function.
+    static void fw_alloc_buffer(uv_handle_t *handle, size_t size,
+                                uv_buf_t *buffer) {
+        auto obj = reinterpret_cast<Adapted *>(handle->data);
+        obj->alloc_buffer(handle, size, buffer);
+    }
+    //! @}
+};
+
+}
+}
--- lib/callme/server.cc
+++ lib/callme/server.cc
@@ -29,22 +29,22 @@ void server::suppress_exceptions(bool suppress) {
     suppress_exceptions_ = suppress;
 }
 
-void server::fw_on_new_connection(uv_stream_t *stream, int status) {
-    server *obj = reinterpret_cast<server *>(stream->data);
-    obj->on_new_connection(stream, status);
-}
+//void server::fw_on_new_connection(uv_stream_t *stream, int status) {
+    //server *obj = reinterpret_cast<server *>(stream->data);
+    //obj->on_new_connection(stream, status);
+//}
 
-void server::fw_on_read(uv_stream_t *stream, ssize_t nread,
-                        const uv_buf_t *buf) {
-    server *obj = reinterpret_cast<server *>(stream->data);
-    obj->on_read(stream, nread, buf);
-}
+//void server::fw_on_read(uv_stream_t *stream, ssize_t nread,
+                        //const uv_buf_t *buf) {
+    //server *obj = reinterpret_cast<server *>(stream->data);
+    //obj->on_read(stream, nread, buf);
+//}
 
-void server::fw_alloc_buffer(uv_handle_t *handle, size_t size,
-                             uv_buf_t *buffer) {
-    server *obj = reinterpret_cast<server *>(handle->data);
-    obj->alloc_buffer(handle, size, buffer);
-}
+//void server::fw_alloc_buffer(uv_handle_t *handle, size_t size,
+                             //uv_buf_t *buffer) {
+    //server *obj = reinterpret_cast<server *>(handle->data);
+    //obj->alloc_buffer(handle, size, buffer);
+//}
 
 void server::on_new_connection(uv_stream_t *stream, int status) {
     LOG_INFO("New connection. Status: %v", status);
