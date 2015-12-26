c385780855722461af383993e7e1ab7e7f2c106e
sztomi <szelei.t@gmail.com>
2015-12-26 13:32:26 +0100

    More client implementation

--- include/callme/client.h
+++ include/callme/client.h
@@ -1,15 +1,16 @@
 #pragma once
 
-#include "callme/detail/string_ref.h"
 #include "callme/maybe.h"
 #include "msgpack.hpp"
+#include "callme/detail/string_ref.h"
+#include "callme/detail/uv_adaptor.h"
 
 namespace callme
 {
 
 //! \brief Implements a client that connects to a msgpack-rpc server and is
 //! able to call functions synchronously or asynchronously.
-class client
+class client : public detail::uv_adaptor<client>
 {
 public:
     client(string_ref addr, uint16_t port);
@@ -36,21 +37,6 @@ private:
     template<typename... Args>
     msgpack::object pack_call(string_ref name, std::tuple<Args...> const& args);
 
-    //! \defgroup Static callbacks that forward calls to members using
-    //! the passed data pointer.
-    //! @{
-    //! \brief Forwards the on_new_connection callback to a member function.
-    static void fw_on_new_connection(uv_stream_t *srv, int status);
-
-    //! \brief Forwards the on_read callback to a member function.
-    static void fw_on_read(uv_stream_t *stream, ssize_t nread,
-                           const uv_buf_t *buf);
-
-    //! \brief Forwards the alloc_buffer callback to a member function.
-    static void fw_alloc_buffer(uv_handle_t *handle, size_t size,
-                                uv_buf_t *buffer);
-    //! @}
-
     //! \brief Handles a new connection
     void on_new_connection(uv_stream_t *stream, int status);
 
@@ -59,6 +45,12 @@ private:
 
     //! \brief Allocates a buffer directly inside the unpacker, avoiding a copy.
     void alloc_buffer(uv_handle_t *handle, size_t size, uv_buf_t *buffer);
+
+    friend class detail::uv_adaptor<client>;
+
+private:
+    uint16_t port_;
+    std::string addr_;
 };
 
 }
---
+++ lib/callme/client.cc
@@ -0,0 +1,12 @@
+#include "uv.h"
+
+namespace callme
+{
+
+client::client(string_ref addr, uint16_t port)
+    : addr_(addr), port_(port)
+{
+
+}
+
+}
