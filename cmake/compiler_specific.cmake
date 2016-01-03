401430e903184e7daa8132c0f6793b40c4e2fb0d
sztomi <szelei.t@gmail.com>
2016-01-02 16:22:20 -0800

    Opps, I actually need the OLD policy setting. Also removed some deprecation warnings and a rouge cin.ignore in the tests. Should be all good now.

--- cmake/compiler_specific.cmake
+++ cmake/compiler_specific.cmake
@@ -1,6 +1,6 @@
 # Compiler-specific options
 
-cmake_policy(SET CMP0054 NEW) # silence quoted variables warning
+cmake_policy(SET CMP0054 OLD) # silence quoted variables warning
 
 if (${CMAKE_CXX_COMPILER_ID} MATCHES "Clang")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -pedantic -std=c++14")
@@ -12,9 +12,9 @@ elseif (${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU")
 elseif (${CMAKE_CXX_COMPILER_ID} STREQUAL "Intel")
   # using Intel C++
 elseif (${CMAKE_CXX_COMPILER_ID} STREQUAL "MSVC")
-    add_definitions(-DWIN32_LEAN_AND_MEAN -D_CRT_SECURE_NO_WARNINGS -D_GNU_SOURCE)
+    add_definitions(-DWIN32_LEAN_AND_MEAN -D_CRT_SECURE_NO_WARNINGS -D_CRT_NONSTDC_NO_DEPRECATE -D_GNU_SOURCE)
 	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /EHsc")
 	set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /Zi")
-    set(CMAKE_EXE_LINKER_FLAGS_DEBUG "${CMAKE_EXE_LINKER_FLAGS_DEBUG} /DEBUG /OPT:REF /OPT:ICF")
+    set(CMAKE_EXE_LINKER_FLAGS_DEBUG "${CMAKE_EXE_LINKER_FLAGS_DEBUG} /DEBUG")
 	STRING(REPLACE "/O2" "/Od" CMAKE_CXX_FLAGS_DEBUG ${CMAKE_CXX_FLAGS_DEBUG})
 endif()
--- tests/testmain.cc
+++ tests/testmain.cc
@@ -2,6 +2,5 @@
 
 int main(int argc, char *argv[]) {
     testing::InitGoogleTest(&argc, argv);
-	std::cin.ignore();
     return RUN_ALL_TESTS();
 }
