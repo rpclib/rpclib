0544276095d8cfd81868b40e15fa10bd89fad894
sztomi <szelei.t@gmail.com>
2016-02-21 22:53:27 +0100

    Removed some unneeded files+added version.h (#3)

--- examples/custom_types/main.cc
+++ examples/custom_types/main.cc
@@ -19,7 +19,7 @@ int main() {
     callme::server s("localhost", 8080);
 
     s.bind("transform", [](point &p) {
-        std::cout << "Transforming " << p << std::endl;
+        std::cout << "Transforming: " << p << std::endl;
         point t;
         t.x = p.x * 2;
         t.y = p.y * 3;
@@ -27,11 +27,7 @@ int main() {
     });
     s.async_run();
 
-    std::cout << sizeof(callme::client) << std::endl;
-    std::cout << alignof(callme::client) << std::endl;
-
     callme::client c("localhost", 8080);
-
     auto result = c.call("transform", point{1, 2}).as<point>();
 
     std::cout << "Transformed: " << result << std::endl;
---
+++ include/callme/version.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#ifndef VERSION_H_UMZFBKB4
+#define VERSION_H_UMZFBKB4
+
+#define CALLME_VERSION_MAJOR 1
+#define CALLME_VERSION_MINOR 0
+#define CALLME_VERSION_BUILD 0
+
+#endif /* end of include guard: VERSION_H_UMZFBKB4 */
