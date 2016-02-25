7305e1d0debfc092bcd120be4f32cfa9bbccd959
sztomi <szelei.t@gmail.com>
2016-02-25 19:48:21 +0100

    Check response data before serializing

--- lib/callme/response.cc
+++ lib/callme/response.cc
@@ -31,11 +31,20 @@ response::response(msgpack::object_handle o) : response() {
 
 msgpack::sbuffer response::get_data() const {
     msgpack::sbuffer data;
-    response_type r(1, id_,
-                    error_.size() > 0 ? msgpack::object(error_)
-                                      : msgpack::object(msgpack::type::nil()),
-                    result_->get());
-    msgpack::pack(&data, r);
+	if (!result_) {
+		auto error_resp = std::make_tuple(1, id_,
+			error_.size() > 0 ? msgpack::object(error_)
+			                  : msgpack::object(msgpack::type::nil()));
+		msgpack::pack(&data, error_resp);
+	}
+	else {
+		response_type r(1, id_,
+			error_.size() > 0 ? msgpack::object(error_)
+			: msgpack::object(msgpack::type::nil()),
+			result_->get());
+		msgpack::pack(&data, r);
+	}
+
     return data;
 }
 
