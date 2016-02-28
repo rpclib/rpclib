b06b86a830fb7c67aecaf6e21a024e4bd8677ebd
sztomi <szelei.t@gmail.com>
2016-02-28 19:57:04 +0100

    Release script changes

--- utils/make_release_linux.sh
+++ utils/make_release_linux.sh
@@ -6,14 +6,14 @@ SRC=src
 ARCH64_DIR=arch_64
 ARCH32_DIR=arch_32
 
-cd $RELEASE_OUTPUT_DIR
+mkdir -p $RELEASE_OUTPUT_DIR && cd $RELEASE_OUTPUT_DIR
 
 echo "Getting sources"
-git clone --depth=1 --branch=branch https://github.com/sztomi/callme.git src
+git clone --depth=1 --branch=dev https://github.com/sztomi/callme.git src
 
 echo "Building for 64-bit"
 # 64-bit
-mkdir ARCH64_DIR && cd $_
+mkdir -p $ARCH64_DIR && cd $ARCH64_DIR
 cmake ../src
 make -j6
 cpack
