d4caad7ba05e280d7e3098be93587ff705479a91
sztomi <szelei.t@gmail.com>
2016-02-28 21:30:06 +0100

    Release script output in the same dir

--- utils/make_release_linux.sh
+++ utils/make_release_linux.sh
@@ -3,42 +3,38 @@
 RELEASE_OUTPUT_DIR=release
 SRC=src
 
-ARCH64_DIR=arch_64
-ARCH32_DIR=arch_32
 BUILD_DIR=build
 CMAKE_FLAGS="-DCALLME_BUILD_TESTS=OFF -DCALLME_BUILD_EXAMPLES=OFF"
 
 # cleanup
 [ -d $RELEASE_OUTPUT_DIR ] && rm -rf $RELEASE_OUTPUT_DIR
-[ -d $ARCH32_DIR ] && rm -rf $ARCH32_DIR
-[ -d $ARCH64_DIR ] && rm -rf $ARCH64_DIR
 
 # create dirs
 mkdir -p $RELEASE_OUTPUT_DIR && cd $RELEASE_OUTPUT_DIR
-mkdir -p $ARCH64_DIR
-mkdir -p $ARCH32_DIR
 
-git clone --depth=1 --branch=dev https://github.com/sztomi/callme.git src
+git clone --depth=1 --branch=dev https://github.com/sztomi/callme.git $SRC
 
 # 64-bit
 mkdir -p $BUILD_DIR && cd $BUILD_DIR
-cmake ../src -DCALLME_FORCE_M64=ON $CMAKE_FLAGS
-make -j6
-cpack
+cmake ../$SRC -DCALLME_FORCE_M64=ON $CMAKE_FLAGS
+make -j8
+cpack -G TGZ
+cpack -G STGZ
 cpack -G DEB
 cd ..
 
-mv $BUILD_DIR/output/pkg/* $ARCH64_DIR
-rm -rf build
+mv $BUILD_DIR/output/pkg/* .
+rm -rf $BUILD_DIR
 
 # 32-bit
 mkdir -p $BUILD_DIR && cd $BUILD_DIR
 cmake ../src -DCALLME_FORCE_M32=ON $CMAKE_FLAGS
-make -j6
-cpack
+make -j8
+cpack -G TGZ
+cpack -G STGZ
 cpack -G DEB
 cd ..
 
-mv $BUILD_DIR/output/pkg/* $ARCH32_DIR
-rm -rf build
-rm -rf src
+mv $BUILD_DIR/output/pkg/* .
+rm -rf $BUILD_DIR
+rm -rf $SRC
