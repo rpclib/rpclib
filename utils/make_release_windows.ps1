74feb5fbf8e75e79f5f1442e61bc153f2a918727
sztomi <szelei.t@gmail.com>
2016-07-31 06:27:07 -0700

    Added name suffix to windows builds

--- utils/make_release_windows.ps1
+++ utils/make_release_windows.ps1
@@ -6,12 +6,11 @@ $BUILD_DIR="build"
 $CMAKE_FLAGS="-DRPCLIB_BUILD_TESTS=OFF"
 
 Remove-Item -Force -Recurse $RELEASE_OUTPUT_DIR 
-Remove-Item -Force -Recurse $SRC
 
 mkdir $RELEASE_OUTPUT_DIR
 pushd $RELEASE_OUTPUT_DIR
 
-git clone --depth=1 --branch=renaming https://github.com/rpclib/rpclib.git $SRC
+git clone --depth=1 --branch=dev https://github.com/rpclib/rpclib.git $SRC
 
 $archs = @("x86"; "x64")
 $configs = @("Debug"; "Release")
@@ -30,29 +29,39 @@ $arch_generators = @{
 }
 
 $extra_cmake_flags = @{
+	"MD"="";
+	"MDd"="";
 	"MT"="-DRPCLIB_MSVC_STATIC_RUNTIME=ON";
 	"MTd"="-DRPCLIB_MSVC_STATIC_RUNTIME=ON"
 }
 
-function build_config($arch, $config, $runtime) {
+function build_config($arch, $runtime, $config) {
 	echo "Building $arch $runtime $config..."
-	$special_build_dir = "build_$arch_$runtime".ToLower()
+	$special_build_dir = "build_$arch$runtime$config".ToLower()
+	echo "Build directory: $special_build_dir"
 	mkdir $special_build_dir
 	pushd $special_build_dir
-	cmake ../$SRC -G "Visual Studio 14 2015 Win64" $CMAKE_FLAGS
-	cmake --build . --config Release
+	
+	$suffix = $runtime_suffixes["$runtime$config"]
+	$gen = $arch_generators[$arch]
+	$flags = $extra_cmake_flags[$suffix]
+	$suffix_lower = $suffix.ToLower()
+	
+	cmake ../$SRC -G $gen $CMAKE_FLAGS $flags -DRPCLIB_NAME_SUFFIX="$suffix_lower"
+	cmake --build . --config $config
+	
 	popd
 }
 
-build_config "x86" "Release" "Dynamic"
-
-popd
+build_config "x64" "Static" "Debug"
 
 foreach ($arch in $archs) {
 	foreach ($config in $configs) {
 		foreach ($runtime in $runtimes) {
-
+			#build_config $arch $runtime $config
 		}
 	}
 }
+
+popd
 
