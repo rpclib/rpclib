7959bf1ce2826a526bfb15236435ef860fc4f8a4
sztomi <szelei.t@gmail.com>
2017-02-16 23:39:31 +0100

    Added config.h for global config values

---
+++ include/rpc/config.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#ifndef CONFIG_H_L7IVDSPZ
+#define CONFIG_H_L7IVDSPZ
+
+#include <stdlib.h>
+
+namespace rpc
+{
+
+static constexpr std::size_t DEFAULT_BUFFER_SIZE = 1024 << 10;
+static constexpr double BUFFER_GROW_FACTOR = 2.0;
+
+} /* rpc */
+
+#ifndef RPCLIB_MSGPACK
+#define RPCLIB_MSGPACK clmdep_msgpack
+#endif /* ifndef RPCLIB_MSGPACK */
+
+#endif /* end of include guard: CONFIG_H_L7IVDSPZ */
