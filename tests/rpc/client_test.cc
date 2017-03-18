2738cc532f4a1563ce05762d6e5cd951ca9cc1df
sztomi <szelei.t@gmail.com>
2017-03-18 16:36:53 +0100

    client: added unit test for timeout

--- tests/rpc/client_test.cc
+++ tests/rpc/client_test.cc
@@ -2,9 +2,12 @@
 
 #include "rpc/client.h"
 #include "rpc/server.h"
+#include "rpc/rpc_error.h"
 #include "testutils.h"
+#include "format.h"
 
 #include <chrono>
+#include <thread>
 
 using namespace rpc::testutils;
 
@@ -17,6 +20,9 @@ public:
         s.bind("dummy_void_multiarg",
                [this](int x, int y) { md.dummy_void_multiarg(x, y); });
         s.bind("large_return", [](std::size_t bytes){ get_blob(bytes); });
+        s.bind("sleep", [](uint64_t ms) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+        });
         s.async_run();
     }
 
@@ -57,4 +63,35 @@ TEST_F(client_test, large_return) {
         blob_size *= 2;
     }
     // no crash is enough
+}
+
+TEST_F(client_test, timeout_setting_works) {
+    rpc::client client("127.0.0.1", test_port);
+    const uint64_t default_timeout = 5000;
+    EXPECT_EQ(client.get_timeout(), default_timeout);
+
+    const uint64_t short_timeout = 50;
+    client.set_timeout(short_timeout);
+
+    EXPECT_EQ(client.get_timeout(), short_timeout);
+    EXPECT_THROW(client.call("sleep", short_timeout + 1), rpc::timeout);
+
+    client.set_timeout(short_timeout * 2);
+    EXPECT_EQ(client.get_timeout(), short_timeout * 2);
+    EXPECT_NO_THROW(client.call("sleep", short_timeout + 1));
+}
+
+TEST_F(client_test, timeout_right_msg) {
+    rpc::client client("127.0.0.1", test_port);
+    const uint64_t short_timeout = 50;
+    try {
+        client.set_timeout(short_timeout);
+        client.call("sleep", short_timeout + 10);
+        FAIL() << "There was no exception thrown.";
+    } catch (rpc::timeout &t) {
+        auto expected_msg = RPCLIB_FMT::format(
+            "rpc::timeout: Timeout of {}ms while calling RPC function '{}'",
+            client.get_timeout(), "sleep");
+        EXPECT_TRUE(str_match(t.what(), expected_msg));
+    }
 }
