ec1c7669758e9a6aaa4c9914c5112f6da8e62af0
tszelei <tamas.szelei@logmein.com>
2017-07-26 14:35:26 +0200

    build: fixed build on macOS and removed warning reported in #142

--- include/rpc/nonstd/optional.hpp
+++ include/rpc/nonstd/optional.hpp
@@ -21,6 +21,7 @@
 #include <cassert>
 #include <stdexcept>
 #include <utility>
+#include <functional>
 
 #define  optional_lite_VERSION "2.0.0"
 
@@ -1066,7 +1067,7 @@ using namespace optional_lite;
 namespace std {
 
 template< class T >
-class hash< nonstd::optional<T> >
+struct hash< nonstd::optional<T> >
 {
 public:
     std::size_t operator()( nonstd::optional<T> const & v ) const optional_noexcept
