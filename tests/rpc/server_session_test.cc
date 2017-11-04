d15ad94dc916181159c9787b046f3b20716c06ef
sztomi <szelei.t@gmail.com>
2017-11-04 23:32:21 +0100

    server_session_test: reduced connection close test iterations on all platforms

--- tests/rpc/server_session_test.cc
+++ tests/rpc/server_session_test.cc
@@ -37,11 +37,7 @@ TEST_F(server_session_test, consume_big_param) {
 }
 
 TEST_F(server_session_test, connection_closed_properly) {
-#ifdef RPCLIB_WIN32
   const unsigned max_tries = 10;
-#else
-  const unsigned max_tries = 1000;
-#endif
   for (unsigned counter = 0; counter < max_tries; ++counter) {
     rpc::client client("localhost", rpc::constants::DEFAULT_PORT);
     auto response = client.call("func");
