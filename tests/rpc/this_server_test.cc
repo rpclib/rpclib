3a2be55038e0c58805edc0f2e4af7b1d8ceea5ba
Quentin Chateau <quentin.chateau@gmail.com>
2019-03-18 23:26:42 +0100

    Reproduce #187

--- tests/rpc/this_server_test.cc
+++ tests/rpc/this_server_test.cc
@@ -1,5 +1,7 @@
 #include "gtest/gtest.h"
 
+#include <future>
+
 #include "rpc/client.h"
 #include "rpc/server.h"
 #include "rpc/this_server.h"
@@ -24,11 +26,29 @@ protected:
     rpc::client c2;
 };
 
-TEST_F(this_server_test, stop) {
+TEST_F(this_server_test, stop_async) {
     s.bind("stop_server", []() { rpc::this_server().stop(); });
     s.async_run();
     c1.call("stop_server");
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    EXPECT_EQ(c1.get_connection_state(),
+              client::connection_state::disconnected);
+    EXPECT_EQ(c2.get_connection_state(),
+              client::connection_state::disconnected);
+}
+
+TEST_F(this_server_test, stop_sync) {
+    s.bind("stop_server", []() { rpc::this_server().stop(); });
+
+    auto handle = std::async(std::launch::async, [this]() {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        c1.call("stop_server");
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    });
+
+    s.run();
+    handle.get();
 
     EXPECT_EQ(c1.get_connection_state(),
               client::connection_state::disconnected);
