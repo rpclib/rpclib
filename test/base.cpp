70335808d1a74f86763a6852955d08fbef5cda7b
Tamás Szelei <szelei.t@gmail.com>
2019-10-27 09:53:52 +0100

    ci: add azure pipelines

--- test/base.cpp
+++ test/base.cpp
@@ -8,6 +8,6 @@ unsigned int Factorial( unsigned int number ) {
 TEST_CASE( "Factorials are computed", "[factorial]" ) {
   REQUIRE( Factorial(1) == 1 );
   REQUIRE( Factorial(2) == 2 );
-  REQUIRE( Factorial(3) == 5 );
+  REQUIRE( Factorial(3) == 6 );
   REQUIRE( Factorial(10) == 3628800 );
 }
